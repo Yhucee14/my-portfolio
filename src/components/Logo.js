9e5e89bc0b2f980cbaaa0028d938dbdae85aff6f
Yhucee14 <uchechukwu7814@gmail.com>
2023-08-11 08:31:35 -0700

    updated the navbar and projects section

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -8,10 +8,10 @@ const MotionLink = motion(Link);
 const Logo = () => {
   return (
     <div className='flex items-center justify-center mt-2'>
-        <MotionLink href='/' className='w-16 h-16 bg-black-100 flex items-center justify-center text-white rounded-full text-xl font-bold'
+        <MotionLink href='/' className='w-16 h-16 bg-black flex items-center justify-center text-white rounded-full text-xl font-bold'
         whileHover={{
             backgroundColor:[
-                'rgba(131,58,180,1)',
+            'rgba(131,58,180,1)',
             'rgba(253,29,29,1)',
             'rgba(252,176,69,1)',
             'rgba(131,58,180,1)',
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,7 @@ import { FeaturedProjects, OtherProjects } from '@/components/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GithubIcon } from '@/components/Icon'
+import { motion } from 'framer-motion'
 
 const projects = () => {
     return (
@@ -25,14 +26,18 @@ const projects = () => {
                         </div>
 
                         {FeaturedProjects.map((pro) => (
-                            <article key={pro} className=' border border-solid p-5 border-black shadow-2xl rounded-3xl  w-full flex items-center justify-between  '>
+                            <motion.article
+                                initial={{ y: 100 }}
+                                whileInView={{ y: 0, transition: { duration: 1.2, ease: 'easeInOut', staggerChildren: 0.08, } }}
+
+                                key={pro} className=' border border-solid p-5 border-black shadow-2xl rounded-3xl  w-full flex items-center justify-between  '>
 
                                 <Link href={pro.link} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
                                     <Image src={pro.img} alt={pro.title} className=' w-full h-auto ' />
                                 </Link>
 
                                 <div className='flex flex-col w-1/2 pl-4 items-start justify-between py-2'>
-                                <h2 className='my-1 w-full text-left text-xl font-bold text-rightBlue'>{pro.type}</h2>
+                                    <h2 className='my-1 w-full text-left text-xl font-bold text-rightBlue'>{pro.type}</h2>
                                     <Link href={pro.link} target='_blank' className='hover:underline underline-offset-3'>
                                         <h2 className='my-1 w-full text-left text-2xl font-bold text-leftBlue'>{pro.title}</h2>
                                     </Link>
@@ -40,14 +45,14 @@ const projects = () => {
                                     <p className='my-2 font-medium text-black-700'>{pro.summary}</p>
 
                                     <div className='mt-2 flex flex-row items-center w-full'>
-                                    <Link href={pro.link} target='_blank' className='mr-4 rounded-lg bg-leftBlue text-white p-2 px-4 text-lg font-semibold'>View Project </Link>
+                                        <Link href={pro.link} target='_blank' className='mr-4 rounded-lg bg-leftBlue text-white p-2 px-4 text-lg font-semibold'>View Project </Link>
                                         <Link href={pro.github} target='_blank' className='w-10'><GithubIcon /> </Link>
-                                      
+
 
                                     </div>
                                 </div>
 
-                            </article>
+                            </motion.article>
                         ))}
                     </div>
 
@@ -57,7 +62,8 @@ const projects = () => {
                         </div>
 
                         {OtherProjects.map((pro) => (
-                            <article key={pro} className=' border border-solid border-black shadow-2xl p-2 rounded-3xl w-full flex flex-col  justify-between  '>
+                            <motion.article initial={{ y: 200 }}
+                                whileInView={{ y: 0, transition: { duration: 1.2, ease: 'easeInOut', staggerChildren: 0.08, } }} key={pro} className=' border border-solid border-black shadow-2xl p-2 rounded-3xl w-full flex flex-col  justify-between  '>
 
                                 <Link href={pro.link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg'>
                                     <Image src={pro.img} alt={pro.title} className=' w-full h-auto rounded-3xl' />
@@ -71,14 +77,14 @@ const projects = () => {
                                     <p className='my-2 font-medium text-black-700'>{pro.summary}</p>
 
                                     <div className='mt-2 flex flex-row items-center w-full'>
-                                    <Link href={pro.link} target='_blank' className='mr-4 rounded-lg bg-leftBlue text-white p-1 px-4 text-lg font-semibold'>View Project </Link>
+                                        <Link href={pro.link} target='_blank' className='mr-4 rounded-lg bg-leftBlue text-white p-1 px-4 text-lg font-semibold'>View Project </Link>
                                         <Link href={pro.github} target='_blank' className='w-10'><GithubIcon /> </Link>
-                                      
+
 
                                     </div>
                                 </div>
 
-                            </article>
+                            </motion.article>
                         ))}
                     </div>
                 </Layout>
